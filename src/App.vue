96527d218977314a681034649da4aea9d3340d5e
tiago canuto <tiagocanutogames@gmail.com>
2024-03-26 16:43:06 -0300

    componentizando a aplicaçao

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <script setup>
-import { reactive } from 'vue';
+  import { reactive } from 'vue';
+  import Cabecalho from './components/Cabecalho.vue';
+  import Formulario from './components/Formulario.vue';
+  import ListaDeTarefas from './components/ListaDeTarefas.vue';
 
-const estado = reactive  //O objeto estado é criado e contém uma propriedade tarefas, que é um array de objetos representando diferentes tarefas.
-({  
-  filtro: 'todas', 
-  tarefaTemp: '',
-  tarefas:   //tarefas, que é um array de objetos representando diferentes tarefas. Cada tarefa possui uma propriedade
-    [
-      {
-        titulo: 'Estudar ES6',    //titulo (uma string que descreve a tarefa) e uma propriedade
-        finalizada: false,        //finalizada (um booleano indicando se a tarefa está finalizada ou não).
-      },
-      {
-        titulo: 'Estudar SASS',   //titulo (uma string que descreve a tarefa) e uma propriedade
-        finalizada: false,        //finalizada (um booleano indicando se a tarefa está finalizada ou não).
-      },
-      {
-        titulo: 'Ir para a academia',   //titulo (uma string que descreve a tarefa) e uma propriedade
-        finalizada: true,               //finalizada (um booleano indicando se a tarefa está finalizada ou não).
-      },
-    ]
-})
+  const estado = reactive  //O objeto estado é criado e contém uma propriedade tarefas, que é um array de objetos representando diferentes tarefas.
+  ({  
+    filtro: 'todas', 
+    tarefaTemp: '',
+    tarefas:   //tarefas, que é um array de objetos representando diferentes tarefas. Cada tarefa possui uma propriedade
+      [
+        {
+          titulo: 'Estudar ES6',    //titulo (uma string que descreve a tarefa) e uma propriedade
+          finalizada: false,        //finalizada (um booleano indicando se a tarefa está finalizada ou não).
+        },
+        {
+          titulo: 'Estudar SASS',   //titulo (uma string que descreve a tarefa) e uma propriedade
+          finalizada: false,        //finalizada (um booleano indicando se a tarefa está finalizada ou não).
+        },
+        {
+          titulo: 'Ir para a academia',   //titulo (uma string que descreve a tarefa) e uma propriedade
+          finalizada: true,               //finalizada (um booleano indicando se a tarefa está finalizada ou não).
+        },
+      ]
+  })
 
   const getTarefasPendentes = () =>
   {
@@ -61,44 +64,11 @@ const estado = reactive  //O objeto estado é criado e contém uma propriedade t
 </script>
 
 <template>
-  <div class="container">
-    <header class="p-5 mb-4 mt-4 bg-light rounded-5">  <!--pading, margin-boton, margin top, cor, borda arredondada, -->
-      <h1>Minhas tarefas</h1>
-      <p>
-        Voce possui {{ getTarefasPendentes().length }} tarefas pendentes
-      </p>
-    </header>  
-    <form @submit.prevent="cadastraTarefa">
-      <div class="row">
-        <div class="col">
-          <input :value="estado.tarefaTemp" @change="evento => estado.tarefaTemp = evento.target.value" required type="text" placeholder="Digite aqui a descriçao da tarefa" class="form-control">
-        </div>
-        <div class="col-md-2">
-          <button type="submit" class="btn btn-primary">Cadastrar</button>
-        </div>
-        <div class="col-md-2">
-          <select @change="evento => estado.filtro = evento.target.value" class="form-control">
-            <option value="todas">Todas tarefas</option>
-            <option value="pendentes">Pendentes</option>
-            <option value="finalizadas">Finalizadas</option>
-          </select>
-        </div>
-      </div>    
-    </form>
-    <ul class="list-group mt-4">
-      <li class="list-group-item" v-for="tarefa in getTarefasFiltadras()">
-        <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada" :id="tarefa.titulo"  type="checkbox">
-        <label :class="{ done: tarefa.finalizada }" class="ms-3" :for="tarefa.titulo">
-          {{ tarefa.titulo }}
-        </label>
-      </li>
-    </ul>
+  <div class="container">    
+    <Cabecalho :tarefas-pendentes="getTarefasPendentes().length"/>
+    <Formulario :trocar-filtro="evento => estado.filtro = evento.target.value" :tarefa-temp="estado.tarefaTemp" :edita-tarefa-temp="evento => estado.tarefaTemp = evento.target.value" :cadastra-tarefa="cadastraTarefa"/>  
+    <ListaDeTarefas :tarefas="getTarefasFiltadras()"/> 
   </div>
 </template>
 
-<style scoped>
-  .done
-  {
-    text-decoration: line-through; /*aplica uma linha através do conteúdo de texto de um elemento.*/
-  }
-</style>
+
